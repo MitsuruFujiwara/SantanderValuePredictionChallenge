4b0ffd2773fb6ba4e818a8e4277d207e2c227f59
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-08-18 09:55:06 +0900

    LB#1

--- 6NewPatterns.py
+++ 6NewPatterns.py
@@ -1,3 +1,5 @@
+# 参考: https://www.kaggle.com/gurchetan1000/6-new-patterns-extending-jiazhen-0-66-on-train
+
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -15,7 +17,15 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def _get_leak(df, cols,extra_feats, lag=0):
+def rewrite_compiled_leak(leak_df, lag):
+    leak_df["compiled_leak"] = 0
+    for i in range(lag):
+        c = "leaked_target_"+str(i)
+        zeroleak = leak_df["compiled_leak"]==0
+        leak_df.loc[zeroleak, "compiled_leak"] = leak_df.loc[zeroleak, c]
+    return leak_df
+
+def _get_leak(df, cols, extra_feats, lag=0):
     f1 = cols[:((lag+2) * -1)]
     f2 = cols[(lag+2):]
     for ef in extra_feats:
@@ -27,8 +37,6 @@ def _get_leak(df, cols,extra_feats, lag=0):
     d2 = df[f2].apply(tuple, axis=1).to_frame().rename(columns={0: 'key'})
 
     d2['pred'] = df[cols[lag]]
-#     d2.to_csv('extra_d2.csv')
-    #d2 = d2[d2.pred != 0] ### to make output consistent with Hasan's function
     d3 = d2[~d2.duplicated(['key'], keep=False)]
     d4 = d1[~d1.duplicated(['key'], keep=False)]
     d5 = d4.merge(d3, how='inner', on='key')
@@ -38,14 +46,12 @@ def _get_leak(df, cols,extra_feats, lag=0):
 
     return d1.merge(d5, how='left', on='key').pred.fillna(0)
 
-def compiled_leak_result(cols, train, transact_cols, l, y):
+def compiled_leak_result(cols, df, transact_cols, l, y):
 
     max_nlags = len(cols)-2
-    train_leak = train[["ID", "target"] + cols]
-    train_leak["compiled_leak"] = 0
-    train_leak["nonzero_mean"] = train[transact_cols].apply(
-        lambda x: np.expm1(np.log1p(x[x!=0]).mean()), axis=1
-    )
+    df_leak = df[["ID", "target"] + cols]
+    df_leak["compiled_leak"] = 0
+    df_leak["nonzero_mean"] = df[transact_cols].apply(lambda x: np.expm1(np.log1p(x[x!=0]).mean()), axis=1)
     scores = []
     leaky_value_counts = []
     leaky_value_corrects = []
@@ -54,45 +60,45 @@ def compiled_leak_result(cols, train, transact_cols, l, y):
     for i in range(max_nlags):
         c = "leaked_target_"+str(i)
         print('Processing lag', i)
-        train_leak[c] = _get_leak(train, cols,l, i)
 
+        df_leak[c] = _get_leak(df, cols,l, i)
         leaky_cols.append(c)
-        train_leak = train.join(
-            train_leak.set_index("ID")[leaky_cols+["compiled_leak", "nonzero_mean"]],
-            on="ID", how="left"
-        )[["ID", "target"] + cols + leaky_cols+["compiled_leak", "nonzero_mean"]]
-        zeroleak = train_leak["compiled_leak"]==0
-        train_leak.loc[zeroleak, "compiled_leak"] = train_leak.loc[zeroleak, c]
-        leaky_value_counts.append(sum(train_leak["compiled_leak"] > 0))
-        _correct_counts = sum(train_leak["compiled_leak"]==train_leak["target"])
+
+        df_leak = df_leak.set_index("ID")[leaky_cols+["compiled_leak", "nonzero_mean"]]
+        df_leak = df.join(df_leak,on="ID", how="left")
+        df_leak = df_leak[["ID", "target"] + cols + leaky_cols+["compiled_leak", "nonzero_mean"]]
+        zeroleak = df_leak["compiled_leak"]==0
+        df_leak.loc[zeroleak, "compiled_leak"] = df_leak.loc[zeroleak, c]
+        leaky_value_counts.append(sum(df_leak["compiled_leak"] > 0))
+        _correct_counts = sum(df_leak["compiled_leak"]==df_leak["target"])
         leaky_value_corrects.append(_correct_counts*1.0/leaky_value_counts[-1])
+
         print("Leak values found in train", leaky_value_counts[-1])
-        print(
-            "% of correct leaks values in train ",
-            leaky_value_corrects[-1]
-        )
-        tmp = train_leak.copy()
+        print("% of correct leaks values in train ", leaky_value_corrects[-1])
+
+        tmp = df_leak.copy()
         tmp.loc[zeroleak, "compiled_leak"] = tmp.loc[zeroleak, "nonzero_mean"]
+
         print('Na count',tmp.compiled_leak.isna().sum())
+
         scores.append(np.sqrt(mean_squared_error(y, np.log1p(tmp["compiled_leak"]).fillna(14.49))))
-        print(
-            'Score (filled with nonzero mean)',
-            scores[-1]
-        )
-    result = dict(
-        score=scores,
-        leaky_count=leaky_value_counts,
-        leaky_correct=leaky_value_corrects,
-    )
-    return train_leak, result
+
+        print('Score (filled with nonzero mean)', scores[-1])
+
+    result = dict(score=scores,
+                  leaky_count=leaky_value_counts,
+                  leaky_correct=leaky_value_corrects,
+                  )
+    return df_leak, result
 
 def main():
     train = pd.read_csv("train.csv")
     test = pd.read_csv("test.csv")
 
     transact_cols = [f for f in train.columns if f not in ["ID", "target"]]
-    y = np.log1p(train["target"]).values
+    trY = np.log1p(train["target"]).values
     test["target"] = train["target"].mean()
+    valY = np.log1p(test["target"]).values
 
     all_df = pd.concat([train, test]).reset_index(drop=True)
     all_df.columns = all_df.columns.astype(str)
@@ -130,14 +136,36 @@ def main():
     l.append(pattern_3160000.columns.values.tolist())
     l.append(pattern_3255483.columns.values.tolist())
 
-    train_leak, result = compiled_leak_result(cols, train, transact_cols, l, y)
-    result = pd.DataFrame.from_dict(result, orient='columns')
+    train_leak, train_result = compiled_leak_result(cols, train, transact_cols, l, trY)
+    leaky_cols = [c for c in train_leak.columns if 'leaked_target_' in c]
+    train_result = pd.DataFrame.from_dict(train_result, orient='columns')
 
-    result.to_csv('train_leaky_stat.csv', index=False)
+    train_result.to_csv('train_leaky_stat.csv', index=False)
+    train_leak.to_csv('train_leak.csv')
 
-    best_score = np.min(result['score'])
-    best_lag = np.argmin(result['score'])
+    best_score = np.min(train_result['score'])
+    best_lag = np.argmin(train_result['score'])
     print('best_score', best_score, '\nbest_lag', best_lag)
+
+    test_leak, test_result = compiled_leak_result(cols, test, transact_cols, l, valY)
+    test_result = pd.DataFrame.from_dict(test_result, orient='columns')
+
+    test_result.to_csv('test_leaky_stat.csv', index=False)
+    test_leak.to_csv('test_leak.csv')
+
+    # get submission data
+    test_leak = rewrite_compiled_leak(test_leak, best_lag)
+
+    test_res = test_leak[leaky_cols+['compiled_leak']].replace(0.0, np.nan)
+
+    test_leak.loc[test_leak["compiled_leak"]==0, "compiled_leak"] = test_leak.loc[test_leak["compiled_leak"]==0, "nonzero_mean"]
+
+    # submission
+    sub = test[["ID"]]
+    sub["target"] = test_leak["compiled_leak"]
+    sub.to_csv("baseline_sub_lag_{best_lag}.csv", index=False)
+
+    print(sub)
 
 if __name__ == '__main__':
     main()
